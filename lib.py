2e49ef6031b8c2b5e158118a92d28fc4b7d3f1d0
Ola-Radosteva <114145594+Ola-Radosteva@users.noreply.github.com>
2023-10-17 16:25:17 +0500

    update count_elements

--- lib.py
+++ lib.py
@@ -21,9 +21,3 @@ def count_common_elements(*lists):
     return common_elements
 
 
-list1 = [1, 2, 3, 4, 5]
-list2 = [3, 4, 5, 6, 7]
-list3 = [5, 6, 7, 8, 9]
-
-result = count_common_elements(list1, list2, list3)
-print("Количество одинаковых элементов:", result)
---
+++ test.py
@@ -0,0 +1,8 @@
+import lib
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [3, 4, 5, 6, 7]
+list3 = [5, 6, 7, 8, 9]
+
+result = lib.count_common_elements(list1, list2, list3)
+print("Количество одинаковых элементов:", result)
